f3a8bdf89ccd0ded507fe2298286f2508da7f97d
Kawabe-Lux <kawabe-lux@posteo.de>
2022-02-08 10:15:49 +0100

    reorder project structure, added tag-gallery

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,9 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <h1>Tag Gallery</h1>
+  <tag-gallery />
+  <span></span>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script setup>
+import TagGallery from './components/tag-gallery.vue'
+</script>
---
+++ src/components/tag-gallery.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul class="gallery">
+  </ul>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.gallery{
+  display: flex;
+  flex-wrap: wrap;
+  border: solid var(--layout-border-width) var(--border-color);
+  background-color: var(--inner-background-color);
+  padding: 1em;
+  margin-left: 1em;
+}
+</style>
